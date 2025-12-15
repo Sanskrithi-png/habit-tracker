bf481766b7fae68d18cdbb7dca13b75f85458ca6
Sanskrithi-png <1ms23et050@msrit.edu>
2025-12-16 00:41:44 +0530

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,8 @@
+function toggleTheme(){
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark"));
+}
+window.onload = () => {
+  if(localStorage.getItem("theme")==="true")
+    document.body.classList.add("dark");
+};
